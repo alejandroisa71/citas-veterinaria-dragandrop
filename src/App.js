e8b049460a9f7f7b996478056941eb5600b6b1ba
Alejandro <alejandroisa71@gmail.com>
2023-07-19 11:48:47 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import Cita from "./components/Cita";
+import Formulario from "./components/Formulario";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 function App() {
+  //Citas en local storage
+  let citasIniciales = JSON.parse(localStorage.getItem("citas"));
+  if (!citasIniciales) {
+    citasIniciales = [];
+    console.log(citasIniciales);
+  }
+
+  const reorder = (list, startIndex, endIndex) => {
+    const result = [...list];
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+    return result;
+  };
+
+  //Arreglo de citas
+  const [citas, setCitas] = useState(citasIniciales);
+  // console.log(citas);
+
+  useEffect(() => {
+    let citasIniciales = JSON.parse(localStorage.getItem("citas"));
+
+    if (citasIniciales) {
+      localStorage.setItem("citas", JSON.stringify(citas));
+    } else {
+      localStorage.setItem("citas", JSON.stringify([]));
+    }
+  }, [citas]);
+
+  //Funcion que tome las citas y agregue la nueva
+  const crearCita = (cita) => {
+    setCitas([...citas, cita]);
+  };
+
+  //Funcion que elimina cita por su id
+  const eliminarCita = (id) => {
+    setCitas(citas.filter((cita) => cita.id !== id));
+  };
+
+  //Mensaje condicional
+  const titulo = citas.length > 0 ? "Administra tus Citas" : "No hay Citas";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Administrador de Pacientes</h1>
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Formulario crearCita={crearCita} />
+          </div>
+          <DragDropContext
+            onDragEnd={(result) => {
+              const { source, destination } = result;
+              if (!destination) {
+                return;
+              }
+              if (
+                source.index === destination.index &&
+                source.droppableId === destination.droppableId
+              ) {
+                return;
+              }
+              setCitas((prevCitas) =>
+                reorder(prevCitas, source.index, destination.index)
+              );
+            }}
+          >
+            <div className="one-half column">
+              <h2>{titulo}</h2>
+              <Droppable droppableId="citas">
+                {(droppableProvided) => (
+                  <ul
+                    className="cita-container"
+                    {...droppableProvided.droppableProps}
+                    ref={droppableProvided.innerRef}
+                  >
+                    {citas.map((cita, index) => (
+                      <Draggable
+                        key={cita.id}
+                        draggableId={cita.id}
+                        index={index}
+                      >
+                        {(draggableProvided) => (
+                          <li
+                            {...draggableProvided.draggableProps}
+                            ref={draggableProvided.innerRef}
+                            {...draggableProvided.dragHandleProps}
+                            className="cita-item"
+                          >
+                            <Cita
+                              cita={cita}
+                              key={cita.id}
+                              eliminarCita={eliminarCita}
+                            />
+                          </li>
+                        )}
+                      </Draggable>
+                    ))}
+                    {droppableProvided.placeholder}
+                  </ul>
+                )}
+              </Droppable>
+            </div>
+          </DragDropContext>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Formulario.js
@@ -0,0 +1,117 @@
+import { useState } from "react";
+import { v4 as uuid } from "uuid";
+import PropTypes from "prop-types";
+
+const Formulario = ({ crearCita }) => {
+  const [cita, actualizarCita] = useState({
+    mascota: "",
+    propietario: "",
+    fecha: "",
+    hora: "",
+    sintomas: "",
+  });
+  const [error, setError] = useState(false);
+
+  const handleChange = (e) => {
+    actualizarCita({
+      ...cita,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const { mascota, propietario, fecha, hora, sintomas } = cita;
+
+  const submitCita = (e) => {
+    e.preventDefault();
+    //Validar
+    if (
+      !mascota.trim() ||
+      !propietario.trim() ||
+      !fecha.trim() ||
+      !hora.trim() ||
+      !sintomas.trim()
+    ) {
+      setError(true);
+      return;
+    }
+
+    setError(false);
+
+    //Asignar un ID
+    cita.id = uuid();
+
+    //Crear Cita
+    crearCita(cita);
+
+    //Reinciar el form
+    actualizarCita({
+      mascota: "",
+      propietario: "",
+      fecha: "",
+      hora: "",
+      sintomas: "",
+    });
+  };
+
+  return (
+    <>
+      <h2>Crear Cita</h2>
+
+      {error ? (
+        <p className="error-alert">Todos los campos son obligatorios</p>
+      ) : null}
+      <form onSubmit={submitCita}>
+        <label htmlFor="">Nombre Mascota</label>
+        <input
+          type="text"
+          name="mascota"
+          className="u-full-width"
+          placeholder="Nombre Mascota"
+          onChange={handleChange}
+          value={mascota}
+        />
+        <label htmlFor="">Nombre Dueño</label>
+        <input
+          type="text"
+          name="propietario"
+          className="u-full-width"
+          placeholder="Nombre Dueño de la Mascota"
+          onChange={handleChange}
+          value={propietario}
+        />
+        <label htmlFor="">Fecha</label>
+        <input
+          type="date"
+          name="fecha"
+          className="u-full-width"
+          onChange={handleChange}
+          value={fecha}
+        />
+        <label htmlFor="">Hora</label>
+        <input
+          type="time"
+          name="hora"
+          className="u-full-width"
+          onChange={handleChange}
+          value={hora}
+        />
+        <label htmlFor="">Síntomas</label>
+        <textarea
+          name="sintomas"
+          className="u-full-width"
+          onChange={handleChange}
+          value={sintomas}
+        ></textarea>
+        <button type="submit" className="u-full-width button-primary">
+          Agregar Cita
+        </button>
+      </form>
+    </>
+  );
+};
+
+Formulario.propTypes = {
+  crearCita: PropTypes.func.isRequired,
+};
+
+export default Formulario;
